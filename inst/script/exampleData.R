d655583fbe18090d63d73ab324293a536337e75e
Zhenxing Guo <zhenxingguo@ZhenxingtekiMacBook-Pro.local>
2021-07-12 23:16:48 -0400

    Add existing file

--- inst/script/exampleData.R
+++ inst/script/exampleData.R
@@ -1,4 +1,5 @@
 #### here is a simple pipeline of how to generate data saved in "inst/extdata/"
+
 ## Take bam files in datasetTRES
 IP.file = c("cb_ip_rep1_chr19.bam", "cb_ip_rep2_chr19.bam")
 Input.file = c("cb_input_rep1_chr19.bam", "cb_input_rep2_chr19.bam")
