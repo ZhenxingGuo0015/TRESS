a10c54d9b9d1a0c76ffdb56482b5206bc4e3bbc0
Zhenxing Guo <zhenxingguo@ZhenxingtekiMacBook-Pro.local>
2021-10-09 00:20:55 -0400

    delte old files

--- R/findRegion.R
+++
@@ -1,9 +0,0 @@
-## divide the whole genome into consecutive regions,
-## which were sequenced
-findRegion <- function(chr, pos, sep=1000) {
-  pos.diff <- abs(c(as.integer(0), diff(pos)))
-  idx.jump <- which(pos.diff>sep)
-  regions <- rbind(c(1, idx.jump),
-                   c(idx.jump-1, length(pos)))
-  regions
-}
